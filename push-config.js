90b9f71417e5f24c9e93b024d449797856b75b6d
ALAGAPPAN PL <plalagappan2006@gmail.com>
2026-09-18 19:03:36 +0530

    Update VAPID keys in push-config.js

--- push-config.js
+++ push-config.js
@@ -6,4 +6,5 @@
 // your GitHub Actions secret (see README.md, Part 2).
 // ============================================================
 
-export const VAPID_PUBLIC_KEY = "PASTE_YOUR_VAPID_PUBLIC_KEY";
+export const VAPID_PUBLIC_KEY = "BO6IEKl5NUYnCvAj-Qy-E0S3KgKjw1KqrKvxlMSvkMDefkroVQFFFzbwrDc1iLXKPIPKD1s97h2I7AP4zNAuOhY";
+export const VAPID_PRIVATE_KEY = "hoNLsZhG_8gcKWSVDuOSD5xTPihDZnPuB7PLyU0MNnE";
