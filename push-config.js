d16e0ae41b830a61289b50be72bfaf07010396ff
ALAGAPPAN PL <plalagappan2006@gmail.com>
2026-09-20 21:03:00 +0530

    Remove VAPID_PRIVATE_KEY export
    Removed the VAPID_PRIVATE_KEY export from push-config.js.

--- push-config.js
+++ push-config.js
@@ -7,4 +7,3 @@
 // ============================================================
 
 export const VAPID_PUBLIC_KEY = "BO6IEKl5NUYnCvAj-Qy-E0S3KgKjw1KqrKvxlMSvkMDefkroVQFFFzbwrDc1iLXKPIPKD1s97h2I7AP4zNAuOhY";
-export const VAPID_PRIVATE_KEY = "hoNLsZhG_8gcKWSVDuOSD5xTPihDZnPuB7PLyU0MNnE";
